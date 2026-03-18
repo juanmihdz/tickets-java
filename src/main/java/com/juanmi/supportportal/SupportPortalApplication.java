cc72c1202c4b648899d39367a9794e8db4a3c807
juanmihdz <juanmiguelhernandezgomez@gmail.com>
2026-03-18 12:18:07 +0100

    creating project

---
+++ src/main/java/com/juanmi/supportportal/SupportPortalApplication.java
@@ -0,0 +1,12 @@
+package com.juanmi.supportportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportPortalApplication.class, args);
+    }
+}
